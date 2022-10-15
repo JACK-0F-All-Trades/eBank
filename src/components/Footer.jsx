59d400003182aa357079c99017559b84520c439f
JACK-0F-All-Trades <abdullah.25.sharif@gmail.com>
2022-10-15 13:22:07 +0500

    trying some changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,35 +3,30 @@ import { logo } from "../assets";
 import { footerLinks, socialMedia } from "../constants";
 
 const Footer = () => (
-  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
-    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
-      <div className="flex-[1] flex flex-col justify-start mr-10">
-        {/* <img
-          src={logo}
-          alt="hoobank"
-          className="w-[266px] h-[72.14px] object-contain"
-        /> */}
+  <section className={`flex flex-col`}>
+    <div className={`flex-[1] sm:flex-row flex-col flex justify-between mt-10`}>
+      <div>
         <div className="text-[70px] font-semibold">
-        <span className="text-gradient">e</span><span className="text-white">Ba</span><span className="text-gradient">nk</span>
-      </div>
-        <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
+          <span className="text-gradient">e</span>
+          <span className="text-white">Ba</span>
+          <span className="text-gradient">nk</span>
+        </div>
+
+        <p className={`${styles.paragraph} max-w-[312px]`}>
           A new way to make the payments easy, reliable and secure.
         </p>
       </div>
 
-      <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
-        {footerLinks.map((footerlink) => (
-          <div key={footerlink.title} className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}>
-            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
-              {footerlink.title}
-            </h4>
-            <ul className="list-none mt-4">
-              {footerlink.links.map((link, index) => (
+      {/* now for footer links */}
+      <div className={`flex flex-col sm:flex-row justify-center`}>
+        {footerLinks.map((footerLink) => (
+          <div key={footerLink.title} className={`flex flex-col ml-5`}>
+            <h4 className={`text-white cursor-pointer`}>{footerLink.title}</h4>
+            <ul className={`mt-4`}>
+              {footerLink.links.map((link) => (
                 <li
                   key={link.name}
-                  className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
-                    index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
-                  }`}
+                  className={`text-dimWhite cursor-pointer hover:text-white`}
                 >
                   {link.name}
                 </li>
@@ -42,7 +37,9 @@ const Footer = () => (
       </div>
     </div>
 
-    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+    <div></div>
+
+    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45] mt-10">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
         Copyright Ⓒ 2022 eBank. All Rights Reserved.
       </p>
@@ -62,6 +59,65 @@ const Footer = () => (
       </div>
     </div>
   </section>
+  // <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+  //   <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+  //     <div className="flex-[1] flex flex-col justify-start mr-10">
+  //       {/* <img
+  //         src={logo}
+  //         alt="hoobank"
+  //         className="w-[266px] h-[72.14px] object-contain"
+  //       /> */}
+  //       <div className="text-[70px] font-semibold">
+  //       <span className="text-gradient">e</span><span className="text-white">Ba</span><span className="text-gradient">nk</span>
+  //     </div>
+  //       <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
+  //         A new way to make the payments easy, reliable and secure.
+  //       </p>
+  //     </div>
+
+  //     <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+  //       {footerLinks.map((footerlink) => (
+  //         <div key={footerlink.title} className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}>
+  //           <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
+  //             {footerlink.title}
+  //           </h4>
+  //           <ul className="list-none mt-4">
+  //             {footerlink.links.map((link, index) => (
+  //               <li
+  //                 key={link.name}
+  //                 className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
+  //                   index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
+  //                 }`}
+  //               >
+  //                 {link.name}
+  //               </li>
+  //             ))}
+  //           </ul>
+  //         </div>
+  //       ))}
+  //     </div>
+  //   </div>
+
+  //   <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+  //     <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
+  //       Copyright Ⓒ 2022 eBank. All Rights Reserved.
+  //     </p>
+
+  //     <div className="flex flex-row md:mt-0 mt-6">
+  //       {socialMedia.map((social, index) => (
+  //         <img
+  //           key={social.id}
+  //           src={social.icon}
+  //           alt={social.id}
+  //           className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+  //             index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+  //           }`}
+  //           onClick={() => window.open(social.link)}
+  //         />
+  //       ))}
+  //     </div>
+  //   </div>
+  // </section>
 );
 
 export default Footer;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#1652f0",
+        primary: "#242933",
         secondary: "#f0f6ff",
         dimWhite: "rgba(255, 255, 255, 0.7)",
         dimBlue: "rgba(9, 151, 124, 0.1)",
